f9857c66bce97dcd760c5a4241e6e1c9e35bdf58
gyh7790 <gyh7790@qq.com>
2020-06-16 00:26:43 +0800

    提交

--- src/views/system/home/main.vue
+++ src/views/system/home/main.vue
@@ -11,6 +11,12 @@
 </template>       
 
 <style lang="scss">
+    .layout {
+        height: 100%;
+    }
+    .el-container {
+        height: 100%;
+    }
     .el-header, .el-footer {
         background-color: #B3C0D1;
         color: #333;
@@ -28,8 +34,6 @@
         color: #333;
         text-align: center;
         line-height: 160px;
-    }
-    body > .el-container {
-        margin-bottom: 40px;
+        padding: 5px;
     }
 </style>
--- src/views/system/login/login.vue
+++ src/views/system/login/login.vue
@@ -1,5 +1,65 @@
 <template>
-  <div id="app">
-    sgftsdfsdfsd
-  </div>
+  <el-card class="login-from-card">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="ruleForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        ruleForm: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            { required: true, message: '请输入用户名称', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+  
+  .login-from-card {
+    width: 480px;
+    margin: auto;
+    margin-top: 20%;
+  }
+</style>
